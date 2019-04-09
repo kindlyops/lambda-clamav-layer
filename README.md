9a169442be34ca0d8b7f8b3aef6a383e7c09261b
Elliot Murphy <statik@users.noreply.github.com>
2019-04-09 11:12:47 -0400

    Checkpoint layer build.
    Signed-off-by: Elliot Murphy <statik@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # lambda-clamav-layer
 an AWS Lambda Layer with clamav binaries
+
+To generate the layer, run
+
+    docker-compose run layer
+
+This will create a new layer file at build/lambda_layer.zip
+This zip file contains the clamav binaries, along with the
+shared libraries for json-c and pcre that are needed at runtime.
+
+To inspect the contents of the zipfile, run
+
+    unzip -vl build/lambda_layer.zip
+
+To publish the lambda layer, run:
+
+    TODO
---
+++ build/build.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo "prepping clamav"
+
+yum update -y
+amazon-linux-extras install epel -y
+yum install -y cpio yum-utils zip
+
+# extract binaries for clamav, json-c, pcre
+mkdir -p /tmp/build
+pushd /tmp/build
+yumdownloader -x \*i686 --archlist=x86_64 clamav clamav-lib clamav-update json-c pcre2
+rpm2cpio clamav-0*.rpm | cpio -vimd
+rpm2cpio clamav-lib*.rpm | cpio -vimd
+rpm2cpio clamav-update*.rpm | cpio -vimd
+rpm2cpio json-c*.rpm | cpio -vimd
+rpm2cpio pcre*.rpm | cpio -vimd
+popd
+
+mkdir -p bin
+
+cp /tmp/build/usr/bin/clamscan /tmp/build/usr/bin/freshclam /tmp/build/usr/lib64/* bin/.
+cp freshclam.conf bin/freshclam.conf
+
+zip -r9 /opt/app/lambda_layer.zip bin
