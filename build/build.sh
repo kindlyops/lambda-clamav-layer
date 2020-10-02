54d6a4048124b78522525271cf8243041d04b4b1
Seth Sacher <ssacher@collabralink.com>
2020-10-02 10:50:01 -0400

    Download correct libcrypt

--- build/build.sh
+++ build/build.sh
@@ -16,9 +16,15 @@ yum install -y cpio yum-utils zip
 mkdir -p /tmp/build
 pushd /tmp/build
 
+# Download the clamav package that includes unrar
 curl -L --output clamav-0.102.4-14050.el7.art.x86_64.rpm http://www6.atomicorp.com/channels/atomic/centos/7/x86_64/RPMS/clamav-0.102.4-14050.el7.art.x86_64.rpm
 rpm2cpio clamav-0*.rpm | cpio -vimd
 
+# Download libcrypt.so.1
+curl -L --output glibc-2.17-307.el7.1.x86_64.rpm http://mirror.centos.org/centos/7/os/x86_64/Packages/glibc-2.17-307.el7.1.x86_64.rpm
+rpm2cpio glibc*.rpm | cpio -vimd
+
+# Download other package dependencies
 yumdownloader -x \*i686 --archlist=x86_64 clamav clamav-lib clamav-update json-c pcre2 libxml2 bzip2-libs libtool-ltdl xz-libs libprelude gnutls nettle libcurl libnghttp2 libidn2 libssh2 openldap libffi krb5-libs keyutils-libs libunistring cyrus-sasl-lib nss nspr libselinux openssl-libs libcrypt
 rpm2cpio clamav-lib*.rpm | cpio -vimd
 rpm2cpio clamav-update*.rpm | cpio -vimd
@@ -60,8 +66,6 @@ cp /tmp/build/usr/bin/clamscan /tmp/build/usr/bin/freshclam bin/.
 cp -R /tmp/build/usr/lib64/* lib/.
 cp -R /tmp/build/lib64/* lib/.
 cp freshclam.conf bin/freshclam.conf
-
-mv lib/libcrypt-2.26.so lib/libcrypt.so.1
 
 zip -r9 /opt/app/lambda_layer.zip bin
 zip -r9 /opt/app/lambda_layer.zip lib
